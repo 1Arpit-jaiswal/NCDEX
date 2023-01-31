f86eda07d073cf5e689f2ba339492fad902ca63d
arpit <arpitjaiswal2302@gmail.com>
2023-01-31 10:24:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -22,8 +22,9 @@ import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
     <Router>
+       {/* <Navbar/> */}
 {/*       
-      <Navbar/>
+     
       
       <LineChart/>
       <CardSlider/>
